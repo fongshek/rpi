27f4bb98606f9b35b4a0e60f03da17f484c5afdb
fongshek <info.fongming@gmail.com>
2015-08-24 01:54:35 +0800

    add websocket mqtt

---
+++ test/mqtt_ws/config.js
@@ -0,0 +1,13 @@
+// host = '172.16.153.122';	// hostname or IP address
+//host = '127.0.0.1';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+//port = 9001;
+host = 'test.mosquitto.org'
+port = 8080;
+topic = 'test/topic';		// topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+// username = "jjolie";
+// password = "aa";
+cleansession = true;
